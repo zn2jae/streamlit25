052a4ce4621b4bfec837f3164f99badf4b5f7076
Jaejoon Lee <dlwownstablet@gmail.com>
2025-05-14 14:15:05 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("😚 Group2 project")
 st.caption("This is a group project of G2")
 
-st.write("This should be a collaboratory work")
+st.write("This project is in progress. Please wait for only few days.")
 st.markdown("---")
