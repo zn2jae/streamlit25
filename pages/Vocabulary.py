ea2974c7e4713008863138370154312c242abc3a
Jaejoon Lee <dlwownstablet@gmail.com>
2025-05-14 14:46:34 +0900

    Update Vocabulary.py

--- pages/Vocabulary.py
+++ pages/Vocabulary.py
@@ -6,7 +6,7 @@ import random
 
 st.write("🌱 Vocabulary learning")
 
-tab1, tab2, tab3 = st.tabs(["❄️ 1. Lesson: Word list", "❄️ 2. Activity: Listen to the word", "❄️ 3. Spelling practice"])
+tab1, tab2, tab3, tab4 = st.tabs(["❄️ 1. Lesson: Word list", "❄️ 2. Activity: Listen to the word", "❄️ 3. Spelling practice", "❄️ 4. TBA"])
 
 ######### TAB 1
 
@@ -97,3 +97,8 @@ with tab3:
             st.success("✅ Correct!")
         else:
             st.error("❌ Try again.")
+
+######### TAB 4
+
+with tab4:
+  st.caption("It is in progress.")
